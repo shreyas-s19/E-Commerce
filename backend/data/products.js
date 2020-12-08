e969191a6c7e3a92be41aa4382eb4bc0443c4128
shreyas-s19 <65349480+shreyas-s19@users.noreply.github.com>
2020-12-08 12:51:49 +0530

    First commit

---
+++ backend/data/products.js
@@ -0,0 +1,82 @@
+const products = [
+  {
+    _id: '1',
+    name: 'AGV Helmet',
+    image: '/images/Helmet.jpg',
+    description:
+      'AGV is an Italian motorcycle helmet manufacturer active in motorcycle sport',
+    brand: 'AGV',
+    category: 'Gears',
+    price: 24999,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: '2',
+    name: 'MT Helmet',
+    image: '/images/MtHelmet.jpg',
+    description:
+      'MT (Manufacturas Tomas) Helmets are made by the largest motorcycle helmet company in Spain and is one of the most reputed brands in Europe. These helmets are ECE certified which is the highest available safety standard in the world.',
+    brand: 'MT',
+    category: 'Gears',
+    price: 14999,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    _id: '3',
+    name: 'Alpinestars Jacket',
+    image: '/images/Jacket.jpg',
+    description:
+      'Alpinestars is a manufacturer of clothing and protective gear for motorsports and action sports',
+    brand: 'Alpinestars',
+    category: 'Protective gear',
+    price: 19999,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 12,
+  },
+  {
+    _id: '4',
+    name: 'Alpinestars Riding Pants',
+    image: '/images/Riding-pant.jpg',
+    description:
+      'Alpinestars is a manufacturer of clothing and protective gear for motorsports and action sports',
+    brand: 'Alpinestars',
+    category: 'Protective gear',
+    price: 12999,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    _id: '5',
+    name: 'Gloves',
+    image: '/images/Gloves.jpg',
+    description:
+      'Alpinestars is a manufacturer of clothing and protective gear for motorsports and action sports',
+    brand: 'Alpinestars',
+    category: 'Protective Gears',
+    price: 6299,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 10,
+  },
+  {
+    _id: '6',
+    name: 'Axor Helmets',
+    image: '/images/Axor.jpg',
+    description:
+      'Axor was created to cater premium biking segment with high end products while keeping it cost effective yet uncompromising on the quality and safety standards of the helmets.',
+    brand: 'Axor',
+    category: 'Gears',
+    price: 9599,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 12,
+  },
+]
+
+module.exports= products
